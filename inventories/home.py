408b00957cb77d1e72b7d2897ca906e9129682d8
Sanyam Kapoor <sanyamk@nvidia.com>
2026-02-03 15:16:48 -0500

    separate optional and required inventory vars with .get calls

--- inventories/home.py
+++ inventories/home.py
@@ -6,13 +6,16 @@ mac = (
             "@local",
             dict(
                 backup_dir="~/Cloud\ Drive/Credentials",
-                vault_pass=config.INVENTORY_VARS["VAULT_PASS"],
                 cloudflare_email=config.INVENTORY_VARS["EMAIL"],
                 cloudflare_api_key=config.INVENTORY_VARS["CLOUDFLARE_API_KEY"],
-                goatcounter_site=config.INVENTORY_VARS["GOATCOUNTER_SITE"],
-                mapbox_access_token=config.INVENTORY_VARS["MAPBOX_ACCESS_TOKEN"],
-                discord_webhook_token=config.INVENTORY_VARS["DISCORD_WEBHOOK_TOKEN"],
-                hcloud_token=config.INVENTORY_VARS["HCLOUD_TOKEN"],
+                discord_webhook_token=config.INVENTORY_VARS.get("DISCORD_WEBHOOK_TOKEN"),
+                goatcounter_site=config.INVENTORY_VARS.get("GOATCOUNTER_SITE"),
+                hcloud_token=config.INVENTORY_VARS.get("HCLOUD_TOKEN"),
+                mapbox_access_token=config.INVENTORY_VARS.get("MAPBOX_ACCESS_TOKEN"),
+                vault_pass=config.INVENTORY_VARS["VAULT_PASS"],
+                wandb_api_key=config.INVENTORY_VARS.get("WANDB_API_KEY"),
+                wandb_username=config.INVENTORY_VARS.get("WANDB_USERNAME"),
+                wandb_entity=config.INVENTORY_VARS.get("WANDB_ENTITY"),
             ),
         )
     ],
@@ -24,8 +27,5 @@ all = (
     dict(
         org="home",
         email=config.INVENTORY_VARS["EMAIL"],
-        wandb_api_key=config.INVENTORY_VARS["WANDB_API_KEY"],
-        wandb_username=config.INVENTORY_VARS["WANDB_USERNAME"],
-        wandb_entity=config.INVENTORY_VARS["WANDB_ENTITY"],
     ),
 )
